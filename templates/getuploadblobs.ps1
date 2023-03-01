5d6804a1e979c382a35347f8a785075db4a77c36
Bogdan Gavrilovic <bgavrilovic@microsoft.com>
2023-03-01 12:42:44 +0100

    Add PS script to pull and push blobs

---
+++ templates/getuploadblobs.ps1
@@ -0,0 +1,35 @@
+try
+  { 
+  	Enable-AzureRmAlias -Scope CurrentUser
+    Install-Module Az.Storage -AllowClobber -Force
+  }
+catch
+  {
+    Write-Output "Skip install"
+  }
+Invoke-WebRequest 'https://github.com/bgavrilovicMS/deploy-bacpac-test/raw/main/data/dogwooddojo.bacpac' -OutFile dogwooddojo.bacpac
+Invoke-WebRequest 'https://github.com/bgavrilovicMS/deploy-bacpac-test/raw/main/data/contosoconcerthall.bacpac' -OutFile contosoconcerthall.bacpac
+Invoke-WebRequest 'https://github.com/bgavrilovicMS/deploy-bacpac-test/raw/main/data/fabrikamjazzclub.bacpac' -OutFile fabrikamjazzclub.bacpac
+Invoke-WebRequest 'https://github.com/bgavrilovicMS/deploy-bacpac-test/raw/main/data/wingtiptenantdb.bacpac' -OutFile wingtiptenantdb.bacpac
+Invoke-WebRequest 'https://github.com/bgavrilovicMS/deploy-bacpac-test/raw/main/data/wingtipcatalogdb.bacpac' -OutFile wingtipcatalogdb.bacpac
+
+
+
+$StorageAccount = Get-AzureRmStorageAccount -ResourceGroupName 'wtdemo-github-27022023' -Name wingtipdemo
+$Context = $StorageAccount.Context
+
+Set-AzureStorageBlobContent `
+-File dogwooddojo.bacpac `
+-Container wingtipdemo -Blob dogwooddojo.bacpac -Context $Context -Force
+Set-AzureStorageBlobContent `
+-File contosoconcerthall.bacpac `
+-Container wingtipdemo -Blob contosoconcerthall.bacpac -Context $Context -Force
+Set-AzureStorageBlobContent `
+-File fabrikamjazzclub.bacpac `
+-Container wingtipdemo -Blob fabrikamjazzclub.bacpac -Context $Context -Force
+Set-AzureStorageBlobContent `
+-File wingtipcatalogdb.bacpac `
+-Container wingtipdemo -Blob wingtipcatalogdb.bacpac -Context $Context -Force
+Set-AzureStorageBlobContent `
+-File wingtiptenantdb.bacpac `
+-Container wingtipdemo -Blob wingtiptenantdb.bacpac -Context $Context -Force
